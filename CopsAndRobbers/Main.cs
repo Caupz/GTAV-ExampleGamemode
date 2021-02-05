e4341b84cbf3f26a5b7aaa86d460246f08e09e77
Caupo Helvik <helvikcaupo@gmail.com>
2021-02-05 21:36:08 +0200

    Added callbacks

--- CopsAndRobbers/Main.cs
+++ CopsAndRobbers/Main.cs
@@ -4,10 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CitizenFX.Core;
+using MPEventFramework;
 
 namespace CopsAndRobbers
 {
     public class Main : BaseScript
     {
+        MPEventFramework.Main Events = new MPEventFramework.Main();
+
+        public Main()
+        {
+            Events.OnTryingToEnterVehicle += OnTryingToEnterVehicle;
+            Events.OnPlayerEnteredVehicle += OnPlayerEnteredVehicle;
+            Events.OnPlayerLeaveVehicle += OnPlayerLeaveVehicle;
+        }
+
+        public void OnTryingToEnterVehicle(int vehicleHandle)
+        {
+            Debug.WriteLine("CNR: OnTryingToEnterVehicle");
+        }
+
+        public void OnPlayerEnteredVehicle(int vehicleHandle, int vehicleSeat)
+        {
+            Debug.WriteLine("CNR: OnPlayerEnteredVehicle");
+        }
+
+        public void OnPlayerLeaveVehicle(int vehicleHandle, int vehicleSeat)
+        {
+            Debug.WriteLine("CNR: OnPlayerLeaveVehicle");
+        }
     }
 }
