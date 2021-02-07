4e526a65a1f30a7a4a005e8578883de3fb3ab778
Caupo Helvik <helvikcaupo@gmail.com>
2021-02-07 19:59:21 +0200

    Added commands for testing

--- CopsAndRobbers/Main.cs
+++ CopsAndRobbers/Main.cs
@@ -5,12 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using CitizenFX.Core;
 using MPEventFramework;
+using static CitizenFX.Core.Native.API;
 
 namespace CopsAndRobbers
 {
     public class Main : BaseScript
     {
         Base core = new Base();
+
         public Main()
         {
             Debug.WriteLine("MAIN SUBSCRIBING TO EVENTS");
@@ -19,8 +21,8 @@ namespace CopsAndRobbers
             core.OnPlayerLeaveVehicle += OnPlayerLeaveVehicle;
             core.OnPlayerSeatChange += OnPlayerSeatChange;
             core.OnPlayerSpawnIntoVehicle += OnPlayerSpawnIntoVehicle;
+            
             Debug.WriteLine("MAIN SUBSCRIBED TO EVENTS");
-            core.enableRealtimeGametime = true;
             // TODO eventid teha ja listenida.
             // TODO testida nii palju evente kui saab.
         }
@@ -54,6 +56,64 @@ namespace CopsAndRobbers
         async Task OnTickMain()
         {
             core.Process();
+        }
+
+        bool helmState = false;
+
+        private void OnClientResourceStart(string resourceName)
+        {
+            RegisterCommand("cuffs", new Action<int, List<object>, string>(async (source, args, raw) =>
+            {
+                if(IsPedCuffed(core.pedHandle))
+                {
+                    SetEnableHandcuffs(core.pedHandle, false);
+                }
+                else
+                {
+                    SetEnableHandcuffs(core.pedHandle, true);
+                }
+            }), false);
+
+            RegisterCommand("weapon", new Action<int, List<object>, string>(async (source, args, raw) =>
+            {
+                int weapon = 0;
+
+                if (Int32.TryParse(args[0].ToString(), out weapon))
+                {
+                    SetEntityHealth(core.pedHandle, weapon);
+                }
+
+                if(weapon < MPEventFramework.Weapon.weaponNames.Count)
+                {
+                    GiveWeaponToPed(core.pedHandle, (uint)GetHashKey(MPEventFramework.Weapon.weaponNames[weapon]), 1000, false, true);
+                }
+            }), false);
+
+            RegisterCommand("hp", new Action<int, List<object>, string>(async (source, args, raw) =>
+            {
+                int hp = 0;
+
+                if(Int32.TryParse(args[0].ToString(), out hp))
+                {
+                    SetEntityHealth(core.pedHandle, hp);
+                }
+            }), false);
+
+            RegisterCommand("armour", new Action<int, List<object>, string>(async (source, args, raw) =>
+            {
+                int arm = 0;
+
+                if (Int32.TryParse(args[0].ToString(), out arm))
+                {
+                    SetPedArmour(core.pedHandle, arm);
+                }
+            }), false);
+
+            RegisterCommand("helm", new Action<int, List<object>, string>(async (source, args, raw) =>
+            {
+                helmState = !helmState;
+                SetPedConfigFlag(core.pedHandle, 35, helmState);
+            }), false);
         }
     }
 }
