4c466473f72c8a70474bb72cb3839bde25eb6eed
Caupo Helvik <helvikcaupo@gmail.com>
2021-02-12 20:17:18 +0200

    Serverside project added

---
+++ CopsAndRobbersServer/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using MPFrameworkServer;
+
+namespace CopsAndRobbersServer
+{
+    public class Main : BaseScript
+    {
+        Server server = new Server();
+
+        public Main()
+        {
+            Debug.WriteLine("======== CopsAndRobbersServer Main ========");
+            server.OnPlayerSpawned += OnPlayerSpawned;
+            Debug.WriteLine("======== CopsAndRobbersServer Main END ========");
+        }
+
+        public void OnPlayerSpawned(Player client)
+        {
+            Debug.WriteLine("CNR OnPlayerSpawned "+client.Handle);
+        }
+    }
+}
